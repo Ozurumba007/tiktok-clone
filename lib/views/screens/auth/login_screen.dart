38a8281aea7ade96d229075fa31cad19eb6310d9
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-16 11:29:07 +0100

    added the email, password, button and 'dont have an account? register'.

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -32,6 +32,10 @@ class LoginScreen extends StatelessWidget {
               height: 25,
             ),
             Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
               child: TextInputField(
                 controller: _emailController,
                 labelText: 'Email',
@@ -39,6 +43,68 @@ class LoginScreen extends StatelessWidget {
                 icon: Icons.email_outlined,
               ),
             ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: TextInputField(
+                controller: _passwordController,
+                labelText: 'Password',
+                isObsecure: true,
+                icon: Icons.lock,
+              ),
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 50,
+              decoration: BoxDecoration(
+                color: buttonColor,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              child: InkWell(
+                onTap: () {
+                  print('Login User');
+                },
+                child: const Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Don\'t have an account? ',
+                  style: TextStyle(fontSize: 20),
+                ),
+                InkWell(
+                  onTap: () => print('Navigating user'),
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: buttonColor,
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
