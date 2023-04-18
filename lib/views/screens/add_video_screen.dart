f1baee8bfd9dced5bbd10d39c3bb3018c69f04fb
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-18 11:36:07 +0100

    added a simple dialog to the add_video_screen.dart

--- lib/views/screens/add_video_screen.dart
+++ lib/views/screens/add_video_screen.dart
@@ -4,12 +4,66 @@ import 'package:tiktok_clone/constant.dart';
 class AddVideoScreen extends StatelessWidget {
   const AddVideoScreen({Key? key}) : super(key: key);
 
+  showOptionsDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+              children: [
+                SimpleDialogOption(
+                  onPressed: () {},
+                  child: Row(
+                    children: const [
+                      Icon(Icons.image),
+                      Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: Text(
+                          'Gallery',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () {},
+                  child: Row(
+                    children: const [
+                      Icon(Icons.camera_alt),
+                      Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: Text(
+                          'Camera',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () {},
+                  child: Row(
+                    children: const [
+                      Icon(Icons.cancel),
+                      Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: Text(
+                          'Cancel',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: InkWell(
-          onTap: () {},
+          onTap: () => showOptionsDialog(context),
           child: Container(
             width: 190,
             height: 50,
