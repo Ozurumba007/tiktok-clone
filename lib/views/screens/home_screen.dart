eba65cf01e17c39cc474d11c18520804a140826c
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-18 11:06:43 +0100

    added some function to the bottom navigation bar

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -2,14 +2,30 @@ import 'package:flutter/material.dart';
 
 import '../widgets/custom_icon.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageIdx = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        onTap: (idx) {
+          setState(() {
+            pageIdx = idx;
+          });
+        },
         type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.white,
+        currentIndex: pageIdx,
         items: const [
           BottomNavigationBarItem(
               icon: Icon(Icons.home, size: 30), label: 'Home'),
