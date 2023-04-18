17c626de9ee2b25cfcb1171e68fbbc53cd82f36c
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-18 01:58:41 +0100

    created a user.dart file in the model folder

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  String name;
+  String profilePhoto;
+  String email;
+  String uid;
+
+  User({
+    required this.name,
+    required this.email,
+    required this.uid,
+    required this.profilePhoto,
+  });
+}
