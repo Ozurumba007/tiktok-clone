cde875e94a03917d986689f772547af714b6209d
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-18 01:54:35 +0100

    adding the email and password initializiation to the auth controller

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,3 +1,49 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 
-class AuthController extends GetxController {}
+import '../constant.dart';
+
+class AuthController extends GetxController {
+  // upload to fire base storage
+  Future<String> _uploadToStorage(File image) async {
+    Reference ref = firebaseStorage
+        .ref()
+        .child('profilePics')
+        .child(firebaseAuth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  // registering the user
+  Future<void> registerUser(
+    String username,
+    String email,
+    String password,
+    File? image,
+  ) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        // save out user to our auth and firebase firestore
+        UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        String downloadUrl = await _uploadToStorage(image);
+      }
+    } catch (e) {
+      Get.snackbar(
+        'Error Creating Account',
+        e.toString(),
+      );
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tiktok_clone/constant.dart';
+import 'package:tiktok_clone/controllers/auth_controller.dart';
 
 import 'views/screens/auth/signup_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
 
   runApp(const MyApp());
 }
