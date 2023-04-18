5f6952743ebdd9855a1f834ba4a7e1fd4b3faf71
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-18 11:01:36 +0100

    added bottom navigation bar to the home screen file \ created a custom icon file for a particular icon

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_icon.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, size: 30), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search, size: 30), label: 'Search'),
+          BottomNavigationBarItem(icon: CustomIcon(), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.message, size: 30), label: 'Messages'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person, size: 30), label: 'Profile'),
+        ],
+      ),
+      body: Center(
+        child: Text('Home Screen'),
+      ),
+    );
   }
 }
